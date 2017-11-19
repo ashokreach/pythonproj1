994dbc23b6a6dcc2f8888c3a4d871726bc1948ec
ashokreach <ashok.bnym@gmail.com>
2017-11-18 19:43:03 -0500

    first commit

---
+++ list.py
@@ -0,0 +1,5 @@
+list=('ash',32,'vidhi love you','30')
+print(list[2])
+input("\a Press the enter key to exit.")
+
+
---
+++ welcome.py
@@ -0,0 +1,5 @@
+# hello world program
+
+print ('This is my first lesson,welcome to the lesson 1')
+input("\a Press the enter key to exit.")
+
